5634bbf3eb61faa9e74ba7bcbfb40cc265a0a3a6
lmpaul <paul.lemercier05@gmail.com>
2023-05-04 14:20:35 +0400

    accomodation page

--- src/components/Accomodation/index.jsx
+++ src/components/Accomodation/index.jsx
@@ -1,7 +1,56 @@
+import { useState, useEffect } from 'react'
+import accomodations from '../../data/data.json'
+import styles from './Accomodation.module.css'
+import Collapse from '../Collapse'
+
 function Accomodation() {
-  return(
-    <h1>Accomodation</h1>
-  )
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [accomodation, setAccomodation] = useState({})
+
+  useEffect(() => {
+    const splitUrl = window.location.href.split('/')
+    const id = splitUrl[splitUrl.length - 1]
+    const rightAccomodation = accomodations.find(accomodation => accomodation.id === id)
+    setAccomodation(rightAccomodation)
+    setIsLoading(false)
+  })
+
+  if (isLoading) {
+    return(
+      <h1>Chargement...</h1>
+    )
+  } else {
+    return(
+      <div className={styles.container}>
+        <div className={styles['split-container']}>
+          <div>
+            <h1>{accomodation.title}</h1>
+            <p>{accomodation.location}</p>
+            <div className={styles['tags-container']}>
+              {accomodation.tags.map((tag) => (
+                <p className={styles.tag}>{tag}</p>
+              ))}
+            </div>
+          </div>
+          <div>
+            <div className={styles['host-container']}>
+              <p>{accomodation.host.name}</p>
+              <img className={styles.avatar} src={accomodation.host.picture} alt="host avatar" />
+            </div>
+          </div>
+        </div>
+        <div className={styles['split-container']}>
+          <div className={styles.col}>
+            <Collapse title='Description' text={accomodation.description} margin={false}/>
+          </div>
+          <div className={styles.col}>
+            <Collapse title='Description' text={accomodation.description} margin={false}/>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Accomodation
